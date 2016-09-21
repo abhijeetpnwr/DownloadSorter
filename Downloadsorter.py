045a3d03fb67f4b35323c58ac5331c771e7223db
abhijeet <abhijeetpnwr@gmail.com>
2016-09-21 12:17:43 +0530

    More extension categorised

--- Downloadsorter.py
+++ Downloadsorter.py
@@ -4,13 +4,13 @@ import os
 
 allFiles = os.listdir("/home/abhijeet/Downloads")
 
-imageformats=["jpg","bmp","png","gif"]
-textformats=["txt","fasta","out"]
-pdfs=["pdf"]
-exes=["exe","tar","bz2","bz","a","bat","lib","zip","dll"]
+imageformats=["jpg","bmp","png","gif","svg"]
+textformats=["txt","fasta","out","txt~","vcf"]
+pdfs=["pdf","epub"]
+exes=["exe","tar","bz2","bz","a","bat","lib","zip","dll","deb","so","run"]
 officedocs=["xlsx","xls","doc","docx","ppt"]
-Web=["js","css","html","jsp","xml","c","java","py","pl","CPP","csv","json","h","H","jar","PL","pm"]
-videos=["mpeg","3gp","mp4","avi","flv","mov","srt"]
+Web=["js","css","html","jsp","xml","c","java","sh","py","pl","CPP","csv","json","h","H","jar","PL","pm"]
+videos=["mpeg","3gp","mp4","avi","flv","mov","srt","mkv"]
 songs=["mp3","wav","mpa"]
 
 folderMap={"Image":imageformats,"TextFile":textformats,"PDF":pdfs,"Software_Compressed":exes,"Documents":officedocs,"Programming":Web,"Video":videos,"songs":songs}
@@ -21,15 +21,25 @@ foldernamemap={"imageformats":"Photos","textformats":"Textfiles","pdfs":"Ebooks_
 
 
 for file in allFiles:
+
+
 	namelist = file.split(".")
+
+
 	
 	if len(namelist)>1:
 		extension = namelist[len(namelist)-1]
+
+		extensionfound = False
 
 		for keys in folderMap:
 
 			if extension in folderMap[keys]:
 				print "",file," is :",keys,"\n"
+				extensionfound=True
+
+		if extensionfound==False:
+			print "No file type found for ",file
 		
 	else:
 		print "Not defined :",file,"\n"
